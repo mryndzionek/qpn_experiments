1314558a05fb6092e58d6bbcbf1ad9d9a26b030a
Ryndzionek Mariusz <mryndzionek@gmail.com>
2015-01-18 12:16:41 +0100

    Added nrf24l01 module spectrum analyser example
    Based on existing AVR libraries and Arduino projects

---
+++ examples/nrf/main.c
@@ -0,0 +1,215 @@
+
+#include "config.h"
+#include "mirf.h"
+#include "nRF24L01.h"
+#include "spi.h"
+#include "lcd.h"
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+
+#define BUFFER_SIZE (10)
+#define CHANNELS    (64)
+#define DELAY       (32)
+
+const uint8_t PROGMEM minibars[][8] = {
+        {
+                0b00000,
+                0b00000,
+                0b00000,
+                0b00000,
+                0b00000,
+                0b00000,
+                0b00000,
+                0b01110,
+        },
+        {
+                0b00000,
+                0b00000,
+                0b00000,
+                0b00000,
+                0b00000,
+                0b00000,
+                0b01110,
+                0b01110,
+        },
+        {
+                0b00000,
+                0b00000,
+                0b00000,
+                0b00000,
+                0b00000,
+                0b01110,
+                0b01110,
+                0b01110,
+        },
+        {
+                0b00000,
+                0b00000,
+                0b00000,
+                0b00000,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+        },
+        {
+                0b00000,
+                0b00000,
+                0b00000,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+        },
+        {
+                0b00000,
+                0b00000,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+        },
+        {
+                0b00000,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+        },
+        {
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+                0b01110,
+        },
+};
+
+
+uint16_t channel[CHANNELS];
+uint8_t buffer[BUFFER_SIZE];
+
+void plot_minibars(int location, int strngth)
+{
+    if (strngth>7)
+    {
+        lcd_set_position(LCD_COLS+location);
+        lcd_putchar(8);
+        lcd_set_position(location);
+        lcd_putchar(strngth-7);
+    }
+    else
+    {
+        lcd_set_position(LCD_COLS+location);
+        lcd_putchar(strngth+1);
+        lcd_set_position(location);
+        lcd_putchar(32);
+    }
+}
+
+void scanChannels(void)
+{
+    uint8_t v;
+    uint8_t rfchannel = 0;
+
+    for(rfchannel = 0; rfchannel < CHANNELS; rfchannel++) {
+        mirf_config_register(RF_CH, (128*rfchannel)/CHANNELS);
+
+        // Or focus on a specific channel
+        //mirf_config_register(RF_CH, 2);
+
+        mirf_CE_lo;
+
+        mirf_CSN_lo; // Pull down chip select
+        spi_fast_shift(FLUSH_RX);  // Write cmd to flush rx fifo
+        mirf_CSN_hi; // Pull up chip select
+
+        mirf_CE_hi; // Start listening
+
+        int delaytime = 0;
+        while (delaytime < DELAY) {
+            delaytime++;
+            mirf_read_register(CD, &v, 1);
+            if (v == 1) {
+                PORTD ^= (1 << (PD5));
+                channel[rfchannel]++;
+            }
+        }
+
+    }
+}
+
+void outputChannels(void)
+{
+    int norm = 0;
+    uint8_t rfchannel = 0;
+
+    for(rfchannel=0 ; rfchannel<CHANNELS ; rfchannel++)
+        if( channel[rfchannel]>norm ) norm = channel[rfchannel];
+    for(rfchannel=0 ; rfchannel<CHANNELS ; rfchannel++)
+    {
+        int pos;
+        if( norm!=0 ) pos = (channel[rfchannel]*10)/norm;
+        else     pos = 0;
+        if( pos==0 && channel[rfchannel]>0 ) pos++;
+        if( pos>8 ) pos = 8;
+        plot_minibars(rfchannel/4, pos*2);
+        channel[rfchannel] = 0;
+    }
+}
+
+/*..........................................................................*/
+int main(void)
+{
+    uint8_t v;
+
+    DDRD |= (_BV(PD5) | _BV(PD0));
+
+    lcd_init();
+
+    for(v=1;v<=8;v++)
+        lcd_customchar_P(v, minibars[v-1]);
+
+    mirf_init();
+    // Wait for mirf to come up
+    _delay_ms(50);
+    // Activate interrupts
+    sei();
+    // Configure mirf
+    mirf_config();
+    mirf_config_register(EN_AA, 0x0);
+    //mirf_config_register(RF_SETUP, 0x0f);
+    //mirf_config_register(SETUP_AW, 0x00);
+    mirf_read_register(RF_CH, &v, 1);
+    if(v == mirf_CH)
+    {
+        PORTD ^= (1 << (PD5));
+        _delay_ms(100);
+        PORTD ^= (1 << (PD5));
+    }
+
+    while(1) {
+
+        RX_POWERUP;
+        mirf_CSN_lo; // Pull down chip select
+        spi_fast_shift(FLUSH_RX);  // Write cmd to flush rx fifo
+        mirf_CSN_hi; // Pull up chip select
+        mirf_CE_hi; // Start listening
+
+        scanChannels();
+        outputChannels();
+        PORTD ^= (1 << (PD0));
+    }
+}
